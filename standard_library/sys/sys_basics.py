37a2859b2f7d65a69fb071b4d591895bf350c8db
Serdar Lyon <lyonserdar@gmail.com>
2022-05-04 15:36:39 -0600

    Added sys.version

--- standard_library/sys/sys_basics.py
+++ standard_library/sys/sys_basics.py
@@ -3,6 +3,9 @@ sys_basics.py
 """
 import sys
 
+# Print the Python version
+print(sys.version.split()[0])
+
 # Print the absolute path to the executable binary file for the Python interpreter
 path = sys.executable
 print(path)
